fb281c3c07843942552652274d4c6d71314ae22c
Nightmee <pr1zetyou@gmail.com>
2023-10-09 14:39:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Зарезервовані слова list, dict, 1tuple, @set, Python Game
+
+number1 = '5'
+number2 = 6
+print(type(number1))
+print(type(number2))
+print(id(number1))
+print(id(number2))
+
+print(int(number1) + number2)
+print(number2 - int(number1))
+
+num_1 = 12
+num_2 = 30
+num_3 = 3,14
+
+print(type(str(num_1)))
+print(type(str(num_2)))
+print(type(str(num_3)))
+
