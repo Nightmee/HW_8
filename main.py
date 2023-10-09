04b6520d215cc79ac10d863f618e13b222f08bed
Nightmee <pr1zetyou@gmail.com>
2023-10-09 16:00:09 +0300

    first commit

--- main.py
+++ main.py
@@ -29,3 +29,17 @@ hobby = 'Programming'
 eat = 'sushi'
 
 print(f"\tHello i'm a {name, last_name},I was born in {year} I'm from {country}.\nMy favorite pet is {pet} I also like to read sometimes, \none of my favorite books is {book} \nWhat about games i like to play {play}, hobby is {hobby} \nEat hm i like almost all but {eat} is perfect")
+
+alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+
+Freedom1 = alphabet[10] + alphabet[35] + alphabet[9] + alphabet[9] + alphabet[7] + alphabet[29] + alphabet[25]
+
+print(f"{Freedom1}")
+
+hope = alphabet[15] + alphabet[29] + alphabet[31] + alphabet[9]
+that = alphabet[39] + alphabet[15] + alphabet[1] + alphabet[39]
+you = alphabet[49] + alphabet[29] + alphabet[41]
+used = alphabet[41] + alphabet[37] + alphabet[9] + alphabet[7]
+HTML = alphabet[14] + alphabet[38] + alphabet[24] + alphabet[22]
+
+print(f"I {hope} {that} {you} {used} {HTML}")
